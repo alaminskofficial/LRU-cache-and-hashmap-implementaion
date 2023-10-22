5c0f6204932283ff0bcf7a802fb4af1a8b51d643
Sk Alamin <91690390+alaminskofficial@users.noreply.github.com>
2023-10-22 13:09:40 +0530

    Update Client.java

--- LRUCache/src/com/alamin/Client.java
+++ LRUCache/src/com/alamin/Client.java
@@ -1,1 +1,14 @@
+package com.alamin;
+public class Client {
+    public static void main(String[] args) {
+        CustomHashMap<String, Integer> hm = new CustomHashMap<>();
+        hm.put("one", 1);
+        hm.put("two", 2);
+        hm.put("three", 3);
 
+        System.out.println(hm.size());
+        System.out.println(hm.get("two"));
+        hm.remove("two");
+        System.out.println(hm.size());
+    }
+}
